fbbe72a2a827d607e7d4d926c883f8fb903dcf1a
Shinogi <shinogi_kazuki@cyberagent.co.jp>
2019-06-14 15:00:25 +0900

    MaterialItemテンプレートまで作成

---
+++ Assets/AutoCustomEditor/Editor/AutoCustomMaterialEditor/Item/MaterialItemDrawerBase.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace AutoCustomEditor
+{
+    public abstract class MaterialItemDrawerBase
+    {
+        protected MaterialItemDrawerBase(ItemParameter parameter, MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+        }
+
+        public abstract void Draw(AutoCustomEditorState state);
+
+        protected virtual GUIContent GetGUIContent(string draw)
+        {
+            return new GUIContent(draw);
+        }
+
+        protected T GetValueFromIndex<T>(T[] list, int index)
+        {
+            if (list == null || list.Length <= index)
+            {
+                return default(T);
+            }
+
+            return list[index];
+        }
+    }
+}
